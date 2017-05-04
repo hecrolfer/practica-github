ebe89da2c53fa5b01e4142ade0b896d41f3f3bfe
alumno <alumno@Lliurex-PC>
2017-05-04 11:41:27 +0200

    aaaaaa

---
+++ practicaGitHub/src/prueba1/HolaMundo.java
@@ -0,0 +1,15 @@
+package prueba1;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println(holaMundo() + " " + holaMundo2());
+	}
+
+	public static String holaMundo() {
+		return "Hola";
+	}
+
+	public static String holaMundo2() {
+		return "Mundo!!!";
+	}
+}
